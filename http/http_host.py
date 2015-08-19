6ca01ea1aab714c070dfb67a69cbb631ee4f8efe
chenyoufu <youfu.ok@163.com>
2015-08-19 10:49:53 +0800

    integration spark sql and streaming and kafka using json

--- http/http_host.py
+++ http/http_host.py
@@ -1,7 +1,8 @@
-import datetime
 import sys
+import os
 import json
 import urllib
+from datetime import *
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import udf
@@ -27,20 +28,33 @@ def getSqlContextInstance(sparkContext):
     return globals()['sqlContextSingletonInstance']
 
 
-def dump_dir(topic):
+def dump_file(topic, output):
     path = topic + '/' + datetime.now().strftime("%Y/%m/%d/%H/")
-    name = topic + datetime.now().strftime("%Y%m%d%H%M%S")
-    with open(path+name, "w") as f:
+    if not os.path.exists(path):
+        mkdir_p(path)
+    name = topic + '.' + datetime.now().strftime("%Y%m%d%H%M%S")
+    print path+name
+    with open(path+name, "w+") as f:
         json.dump(output, f, indent=4)
 
 
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5 (except OSError, exc: for Python <2.5)
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
+
 
 def process(time, rdd):
-    try:
-        print "========= %s =========" % str(time)
+    #try:
+    print "========= %s =========" % str(time)
+    if rdd.count() > 0:
         sqlContext = getSqlContextInstance(rdd.context)
-        df = sqlContext.jsonRDD(rdd)
-        print df.collect()
+        lines = rdd.map(lambda x: x[1])
+        df = sqlContext.jsonRDD(lines)
+        df.printSchema()
         total_groups = df.select("dst_group_id").distinct().dropna().collect()
         # Register this DataFrame as a table.
         get_host = udf(extract_host)
@@ -54,10 +68,10 @@ def process(time, rdd):
                 topN_sql = "select host, total_bytes, in_bytes, out_bytes, latency, requests from urls where dst_group_id = %s order by %s desc limit 50" % (group_id.dst_group_id, opt)
                 topN_collect = sqlContext.sql(topN_sql).toJSON().collect()
                 output[opt][group_id.dst_group_id] = list(json.loads(x) for x in topN_collect) 
-        dump_dir("http")
-    except:
-        print "error"
-        pass
+        dump_file("http", output)
+    #except:
+       # print "error"
+       # pass
 
 
 if __name__ == "__main__":
@@ -66,6 +80,8 @@ if __name__ == "__main__":
     ssc = StreamingContext(sc, 5)
     brokers, topic = sys.argv[1:]
     kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
+    #zkQuorum, topic = sys.argv[1:]
+    #kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 3})
     kvs.foreachRDD(process)
     ssc.start()
     ssc.awaitTermination()
